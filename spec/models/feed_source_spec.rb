c67174dbcf388bc20a2a86d14629c9e7dc782b24
Rafael <rafaelrezendecosta@gmail.com>
2018-02-24 12:33:57 -0300

    Fixes feed source spec

--- spec/models/feed_source_spec.rb
+++ spec/models/feed_source_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe FeedSource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#fetch" do
+    it 'returns a valid array of feed entries' do
+      feed_source = FeedSource.create!( name: 'Code-Squad News' ,
+                                        url: 'http://feeds.feedburner.com/CodeSquadNews?format=xml'
+                                      )
+      entries = feed_source.fetch
+      expect(entries.first).to include(:link)
+    end
+  end
 end
